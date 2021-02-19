cf31b5bee630b06951ca745647341c79ad4f53e3
iki1you <vovastr0v@yandex.ru>
2021-02-19 17:33:45 +0500

    mars image

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
 def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('Again')
-    print('UFO came and added this line')
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/promotion')
+def promotion():
+    prm_list = ['Человечество вырастает из детства.',
+                'Человечеству мала одна планета.',
+                'Мы сделаем обитаемыми безжизненные пока планеты.',
+                'И начнем с Марса!',
+                'Присоединяйся!']
+    return '</br>'.join(prm_list)
+
+
+@app.route('/image_mars')
+def image_mars():
+    return f"""<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <title>Привет, Марс!</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="{url_for('static', filename='img/mars.png')}" alt="здесь должна была быть картинка, но не нашлась">
+                        <br>Вот она какая, красная планета.
+                      </body>
+                    </html>"""
 
 
 if __name__ == '__main__':
-    main()
+    app.run(port=8080, host='127.0.0.1')
